c919161c6d766744e45efcf6919f6186eb100aeb
huycodedao <phunghuyk24@gmail.com>
2025-04-22 10:44:00 +0700

    clean hero

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,41 @@
+/**
+ * A reusable CTA button component.
+ * When clicked, it scrolls smoothly to the section with ID "counter",
+ * with a small offset from the top for better visual placement.
+ */
 
 const Button = ({ text, className, id }) => {
   return (
-    <a className={`${className ?? ''} cta-wrapper`}>
+    <a
+      onClick={(e) => {
+        e.preventDefault(); //Dừng liên kết nhảy ngay lập tức
+
+        const target = document.getElementById("counter"); // Tìm phần có ID "counter"
+        
+        // Chỉ cuộn nếu tìm thấy phần đó và một ID được truyền vào
+//  ngăn không cho nút liên lạc cuộn lên trên cùng
+        if (target && id) {
+          const offset = window.innerHeight * 0.15; // 
+
+          // Tính toán vị trí cuộn
+          const top =
+            target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+          // Cuộn đến vị trí đã tính toán
+          window.scrollTo({ top, behavior: "smooth" });
+        }
+      }}
+      className={`${className ?? ""} cta-wrapper`} // Thêm className nếu có
+    >
       <div className="cta-button group">
         <div className="bg-circle" />
         <p className="text">{text}</p>
         <div className="arrow-wrapper">
-          <img src="./public/images/arrow-down.svg" alt="arrow" />
+          <img src="/images/arrow-down.svg" alt="arrow" />
         </div>
       </div>
     </a>
   );
 };
 
-export default Button
+export default Button;
