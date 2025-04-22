007e6bbd1184cbfcce75bc496b43b3b7925b4d1c
huycodedao <phunghuyk24@gmail.com>
2025-04-22 15:00:09 +0700

    logosecsion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import LogoSection from "./components/LogoSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
-
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   );
 };
