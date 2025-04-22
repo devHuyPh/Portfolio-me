d06112789d194607450460dd2b91a7738db23ace
huycodedao <phunghuyk24@gmail.com>
2025-04-22 10:33:13 +0700

    hero 3d model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { words } from "../constants";
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
+  
   return (
     <>
       <section id="hero" className="relative overflow-hidden">
@@ -54,8 +67,12 @@ const Hero = () => {
             </div>
           </header>
 
-				  {/**hero 3dmodel */}
-				  
+          {/**hero 3dmodel */}
+          <figure>
+            <div className="hero-3d-layout">
+              <HeroExperience />
+            </div>
+          </figure>
         </div>
       </section>
     </>
