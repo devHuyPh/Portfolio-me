2cf79a37af2150b225a0ac67df9889ae09d6edb7
huycodedao <phunghuyk24@gmail.com>
2025-04-23 14:27:42 +0700

    Form Contact

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -14,15 +14,15 @@ const Testimonials = () => {
           />
 
           <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
-                      {testimonials.map(({ imgPath, name, mentions, review }) => (
-                          <GlowCard card={{review}}>
+            {testimonials.map(({ imgPath, name, mentions, review }) => (
+              <GlowCard key={name} card={{ review }}>
                 <div className="flex items-center gap-3">
                   <div>
                     <img src={imgPath} alt={name} />
                   </div>
                   <div>
-                                      <p className="font-bold">{name}</p>
-                                      <p className='text-white-50'>{ mentions}</p>
+                    <p className="font-bold">{name}</p>
+                    <p className="text-white-50">{mentions}</p>
                   </div>
                 </div>
               </GlowCard>
